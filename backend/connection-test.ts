4ed67608621b897133bbf00ba8338b310246dbf8
Alexkyky <alexkyku@gmail.com>
2026-02-27 23:26:30 +0400

    TypeORM connection to DB

--- backend/connection-test.ts
+++ backend/connection-test.ts
@@ -1,19 +1,19 @@
-import { DataSource } from "typeorm"
-import dotenv from "dotenv"
-dotenv.config()
+// import { DataSource } from "typeorm"
+// import dotenv from "dotenv"
+// dotenv.config()
 
-const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.DB_HOST as string,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  // database: "test",
-})
+// const AppDataSource = new DataSource({
+//   type: "postgres",
+//   host: process.env.DB_HOST as string,
+//   port: Number(process.env.DB_PORT),
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   // database: "test",
+// })
 
-try {
-  await AppDataSource.initialize()
-  console.log("Data Source has been initialized!")
-} catch (error) {
-  console.error("Error during Data Source initialization", error)
-}
+// try {
+//   await AppDataSource.initialize()
+//   console.log("Data Source has been initialized!")
+// } catch (error) {
+//   console.error("Error during Data Source initialization", error)
+// }
