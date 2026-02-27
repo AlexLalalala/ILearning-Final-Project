2910e564e1fadf774ba029c63530380144857493
Alexkyky <alexkyku@gmail.com>
2026-02-27 17:10:02 +0400

    Added Swagger and dotenv

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import dotenv from 'dotenv'
+dotenv.config()
+
+console.log(process.env)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('ILearning: Inventory Management')
+  .setDescription('Course Project for ILearning. Inventory management system')
+  .setVersion('1.0')
+  .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
